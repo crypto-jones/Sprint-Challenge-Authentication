51e4d82c912901673bf2d6015f735e7ec4591c5a
crypto-jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-04-20 15:05:39 -0700

    technical issuesresolved. base requirements complete

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -16,29 +16,22 @@ const UserSchema = Schema({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 5
   }
 });
 
-// UserSchema.pre('save', function(next) {
-//   // https://github.com/kelektiv/node.bcrypt.js#usage
-//   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-//   // if there is an error here you'll need to handle it by calling next(err);
-//   // Once the password is encrypted, call next() so that your userController and create a user
-// //   bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
-// //     if (err) {
-// //       return next(err);
-// //     }
-// //     this.password = hash;
-
-// //     return next();
-// //   });
-// // });
-
+// https://github.com/kelektiv/node.bcrypt.js#usage
+// Fill this middleware in with the Proper password encrypting, bcrypt.hash()
+// if there is an error here you'll need to handle it by calling next(err);
+// Once the password is encrypted, call next() so that your userController and create a user
 UserSchema.pre('save', function(next) {
-  bcrypt.hash(this.password, SALT_ROUNDS).then(hash => {
-    this.password.hash;
-    next();
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    return next();
   });
 });
 
@@ -47,6 +40,13 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  return bcrypt.compare(plainTextPW, this.password, (err, res) => {
+    if (err) {
+      return callBack(err);
+    } else {
+      callBack(null, res);
+    }
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
